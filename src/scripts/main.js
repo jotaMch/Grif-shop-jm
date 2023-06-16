de384aa4a5cfcf9f1b86ebf6e2f7e6d6eec4ad10
Jânderson Machado <jandersonmachado090@gmail.com>
2023-06-16 17:14:17 -0300

    alteração de conteudo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,58 +1,23 @@
-$(document).ready(function() {
-    $('#carousel-imagens').slick({
-        autoplay: true,
-    });
-
-    $('.menu-hamburger').click(function() {
-        $('nav').slideToggle();
-    })
-
+$(document).ready(function () {
     $('#telefone').mask('(00) 00000-0000')
 
     $('form').validate({
         rules: {
             nome: {
-                required: true
+            required: true
             },
             email: {
                 required: true,
                 email: true
             },
-            telefone: {
+            nome: {
                 required: true
-            },
-            mensagem: {
-                required: true,
-            },
-            veiculoDeInteresse: {
-                required: false,
             }
         },
         messages: {
             nome: 'Por favor, insira seu nome',
             email: 'Por favor, insira seu e-mail',
             mensagem: 'Por favor, insira sua mensagem'
-        },
-        submitHandler: function(form) {
-            console.log(form)
-        },
-        invalidHandler: function(evento, validador) {
-            let camposIncorretos = validador.numberOfInvalids();
-            if (camposIncorretos) {
-                alert(`Existem ${camposIncorretos} campos incorretos`)
-            }
         }
-    })
-
-    $('.lista-veiculos button').click(function() {
-        const destino = $('#contato');
-
-        const nomeVeiculo = $(this).parent().find('h3').text();
-
-        $('#veiculo-interesse').val(nomeVeiculo);
-
-        $('html').animate({
-            scrollTop: destino.offset().top
-        }, 1000)
     })
 })
